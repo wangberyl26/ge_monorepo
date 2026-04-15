f91f4ea3f0d00438d5582d32532b7f0e0681baa6
wangberyl26 <nwang17776@gmail.com>
2026-04-14 20:50:15 -0600

    Godot Handshake Setup.

--- Godot/parallel-engine/Caller.gd
+++ Godot/parallel-engine/Caller.gd
@@ -1,18 +1,13 @@
 extends Node
-
+@export var receiver: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hey")
-	save_as_scene();
+	print("Call sent.")
+	receiver.OnCalled()
+ # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func save_as_scene():
-	var file_path = "res://parallelEngineLevel.tscn"
-	var scene = PackedScene.new()
-	scene.pack(get_node("your_node"))
-	ResourceSaver.save(file_path,scene)
--- Godot/parallel-engine/Receiver.gd
+++ Godot/parallel-engine/Receiver.gd
@@ -3,16 +3,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hey")
-	save_as_scene();
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func save_as_scene():
-	var file_path = "res://parallelEngineLevel.tscn"
-	var scene = PackedScene.new()
-	scene.pack(get_node("your_node"))
-	ResourceSaver.save(file_path,scene)
+func OnCalled(): print("Call received.")
