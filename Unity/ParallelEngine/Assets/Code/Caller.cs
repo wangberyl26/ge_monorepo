0174e3fb1f778f7730771103367ed2c1997f2da9
wangberyl26 <nwang17776@gmail.com>
2026-04-14 19:48:15 -0600

    Unity Handshake set up complete.

---
+++ Unity/ParallelEngine/Assets/Code/Caller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Caller : MonoBehaviour
+{
+    public Receiver receiver;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Calling on Receiver.");
+        receiver.OnCalled();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
